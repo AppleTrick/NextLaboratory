052d213b47c79f9277997d94cedaf1ba72fefba0
ChangHee Park <arrans9591@gmail.com>
2024-05-06 18:30:29 +0900

    next-mdx 적용 방법

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import createMDX from "@next/mdx";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
+  // Optionally, add any other Next.js config below
+  experimental: {
+    appDir: true, // App Router 기능 활성화
+  },
+};
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/, // mdx뿐만 아니라 md도 처리
+  options: {
+    // 필요한 경우 MDX 옵션 및 플러그인 추가
+  },
+});
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
---
+++ src/app/mdx-page/page.tsx
@@ -0,0 +1,11 @@
+import Welcome from "@/markdown/welcome.mdx";
+import Test from "@/markdown/test.md";
+
+export default function Page() {
+  return (
+    <>
+      <Welcome />
+      <Test />
+    </>
+  );
+}
