6816b5a354bf41dbe22009b57f608c2890eb374e
“ParkChangHee” <arrans9591@gmail.com>
2024-07-02 17:56:25 +0900

    구현은 했는데 이미지 개수나, 화면 크기에 영향을 받음 이거 어떻게 해결해야되지?

--- src/app/docs-page/ui-components/InfiniteScrollingImages/page.tsx
+++ src/app/docs-page/ui-components/InfiniteScrollingImages/page.tsx
@@ -9,7 +9,7 @@ const scroll = keyframes`
     transform: translateX(0);
   }
   100% {
-    transform: translateX(-100%);
+    transform: translateX(-153%);
   }
 `;
 
@@ -39,24 +39,22 @@ const InfiniteScrollingImages = () => {
     const track = ref.current;
     if (track) {
       const images = track.querySelectorAll('img');
-      track.appendChild(images[0].cloneNode(true));
-      track.appendChild(images[1].cloneNode(true));
-      track.appendChild(images[2].cloneNode(true));
-      track.appendChild(images[3].cloneNode(true));
-      track.appendChild(images[4].cloneNode(true));
+      images.forEach((image) => {
+        track.appendChild(image.cloneNode(true));
+      });
     }
   }, []);
 
   return (
     <ImageContainer>
       <ImageTrack ref={ref}>
-        <StyledImage src="/imageSample/1.jpg" alt="Image 1" fill />
-        <StyledImage src="/imageSample/2.jpg" alt="Image 2" fill />
-        <StyledImage src="/imageSample/3.jpg" alt="Image 3" fill />
-        <StyledImage src="/imageSample/4.jpg" alt="Image 4" fill />
-        <StyledImage src="/imageSample/5.jpg" alt="Image 5" fill />
-        <StyledImage src="/imageSample/6.jpg" alt="Image 6" fill />
-        <StyledImage src="/imageSample/7.jpg" alt="Image 7" fill />
+        <StyledImage src="/imageSample/1.jpg" alt="Image 1" width={100} height={100} />
+        <StyledImage src="/imageSample/2.jpg" alt="Image 2" width={100} height={100} />
+        <StyledImage src="/imageSample/3.jpg" alt="Image 3" width={100} height={100} />
+        <StyledImage src="/imageSample/4.jpg" alt="Image 4" width={100} height={100} />
+        <StyledImage src="/imageSample/5.jpg" alt="Image 5" width={100} height={100} />
+        <StyledImage src="/imageSample/6.jpg" alt="Image 6" width={100} height={100} />
+        <StyledImage src="/imageSample/7.jpg" alt="Image 7" width={100} height={100} />
       </ImageTrack>
     </ImageContainer>
   );
